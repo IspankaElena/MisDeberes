eb7de17c1e8f81b1621d513a49ce03e2937aae29
Elena <elena.makarova00@gmail.com>
2023-04-25 22:34:57 +0300

    mostrartodosnumeropar

---
+++ Task1/Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N
+// 5->2,4
+// 8->2,4,6,8
+
+Console.WriteLine("Введите число N");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+for(int i=1;i<=a;i++)
+{
+  if(i % 2 == 0) 
+  System.Console.Write(i + " , ");
+}
+
