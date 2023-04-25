2ec1b8d3381eff71caf0e3d75d31416b94157599
Elena <elena.makarova00@gmail.com>
2023-04-25 21:48:41 +0300

    Maxdetres

---
+++ Task1/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает три числа, и выдает максимальное из этих чисел
+// 2, 3, 7
+// 44, 5, 78
+// 22, 3, 9
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+
+System.Console.WriteLine("Максимальное из этих чисел = "+ max);
+
