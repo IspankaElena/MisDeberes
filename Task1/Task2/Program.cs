c777540f57bc8c9bf6a26923dff8e9fb4b740fe3
Elena <elena.makarova00@gmail.com>
2023-05-14 21:20:51 +0300

    Continuamos

--- Task1/Task2/Program.cs
+++ Task1/Task2/Program.cs
@@ -69,8 +69,83 @@
 // Упражнение. Реализация условного оператора
 
 // Предположим, вам нужно быстро определить, имеет ли клиентская покупка права на рекламную скидку. Сведения о акции указывают, что если стоимость покупки превышает 1000 долл. США, покупка имеет право на скидку в размере 100 долл. США. Если сумма покупки составляет 1000 долл. США или меньше, на покупку распространяется скидка в размере 50 долл. США.
+// Сведения о акции указывают, что если стоимость покупки превышает 1000 долл. США, покупка имеет право на скидку в размере 100 долл. США. Если сумма покупки составляет 1000 долл. США или меньше, на покупку распространяется скидка в размере 50 долл. США.
+
 // <evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
 
-int saleAmount = 1001;
-int discount = saleAmount > 1000 ? 100 : 50;
-Console.WriteLine($"Discount: {discount}");
+// int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {discount}");
+
+// Использование встроенного условного оператора
+// Этот код можно еще больше сжать, ликвидировав временную переменную discount.
+
+// int saleAmount = 1001;
+// Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+// Задача по коду: напишите код для отображения результата переворачивания монеты
+
+// Вероятность того, что результатом будет орел либо решка, должна составлять 50 %.
+
+// Random coin = new Random();
+// int flip = coin.Next(0, 2);
+// Console.WriteLine((flip == 0) ? "heads" : "tails");
+
+// string permission = "Admin|Manager";
+// int level = 19;
+// if (permission.Contains("Admin"))
+// {
+//     if (level > 55)
+//     {
+//       Console.WriteLine("Welcome, Super Admin user.");  
+//     }
+//     else 
+//     {
+//         Console.WriteLine("Welcome, Admin user.");
+//     }
+// }
+//     else if (permission.Contains("Manager"))
+// {
+//     if (level >= 20)
+//     {
+//       Console.WriteLine("Contact an Admin for access.");  
+//     }
+//     else 
+//     {
+//         Console.WriteLine("You do not have sufficient privileges.");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("You do not have sufficient privileges.");
+// }
+
+// string используется для вывода слов, фраз и любых других буквенно-цифровых данных, но не для выполнения вычислений с ними;
+// char представляет отдельную букву или цифру;
+// int представляет целое число;
+// decimal для числа с дробным компонентом
+// booltrue/false для значения истины и лжи
+
+// string firstName;
+// firstName = "Bob";
+// Console.WriteLine(firstName);
+// firstName = "Liem";
+// Console.WriteLine(firstName);
+// firstName = "Isabella";
+// Console.WriteLine(firstName);
+// firstName = "Yasmin";
+// Console.WriteLine(firstName);
+
+// Hello, Bob! You have 3 in your inbox. The temperature is 34.4 celsius.
+
+// string name = "Bob";
+// int messages = 3;
+// decimal temperature = 34.4m;
+
+// Console.Write("Hello, ");
+// Console.Write(name);
+// Console.Write("! You have ");
+// Console.Write(messages);
+// Console.Write(" in your inbox. The temperature is ");
+// Console.Write(temperature);
+// Console.Write(" celsius.");
