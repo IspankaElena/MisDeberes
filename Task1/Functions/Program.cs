506a75f463682829ab60a4864cd0a17a3b25fc95
Elena <elena.makarova00@gmail.com>
2023-05-17 08:45:28 +0300

    Виды методов

--- Task1/Functions/Program.cs
+++ Task1/Functions/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// вид1
+// void Method1()
+// {
+//     Console.WriteLine("Autor....")
+// }
+// Method1();
+
+
+
+
+
+
+
+
+
+
+
+// Вид2
+// void Method2(string msg)
+// {
+//      Console.WriteLine(msg);
+// }
+// Method2(msg: "Text of message");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21(msg: "Text", count: 4);
+
+
+
+
+
+
+
+
+// Вид3
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int Year = Method3();
+// Console.WriteLine(Year);
+
+
+
+
+
+
+
+
+
+
+// Вид4
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "gjk");
+// Console.WriteLine(res);
